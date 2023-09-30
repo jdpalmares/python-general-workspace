dd72d16a1db1003bc52805b235f083e7f51d19c2
Jon Daniel Palmares <jdpalmares@gmail.com>
2023-09-30 14:11:03 -0400

    Added python code from AOC 2022 and Cypher training and modified Readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# python-general-workspace
+# Python General Workspace
 Personal workspace for using and studying python and python 3
+
+Might move to another workspace if it gets more to a specific need
+
+To run files, just run the command below in the root directoy:
+`python <directory>\<python file>`
+
+example:
+`python cypherScripts\VignereCypher.py`
---
+++ cypherScripts/VignereCypher.py
@@ -0,0 +1,14 @@
+def decrypt(ciphertext, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    ciphertext_int = [ord(i) for i in ciphertext]
+    plaintext = ''
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
+        plaintext += chr(value + 65)
+    return plaintext
+
+input = 'jcmpfymspu'
+key = 'cyber'
+
+print('decrypting \"'+input+'\" using key \"'+key+'\" = '+decrypt(input, key))
